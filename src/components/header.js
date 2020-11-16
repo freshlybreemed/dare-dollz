1120cc314b4014875f6a4e4d14f950a4a3bd4366
ebrima <ebrima.jobe92@gmail.com>
2020-11-16 16:45:07 -0500

    starter

--- src/components/header.js
+++ src/components/header.js
@@ -2,32 +2,36 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
-const Header = ({ siteTitle }) => (
+const Header = () => (
   <header
     style={{
-      background: `rebeccapurple`,
       marginBottom: `1.45rem`,
     }}
   >
+    <ul>
+      <li>
+        <Link to="">Dollz</Link>
+      </li>
+      <li>
+        <Link to="">Music Videos</Link>
+      </li>
+      <li>
+        <Link to="">Comic</Link>
+      </li>
+      <li>
+        <Link to="">Studio</Link>
+      </li>
+      <li>
+        <Link to="">Shop</Link>
+      </li>
+    </ul>
     <div
       style={{
         margin: `0 auto`,
         maxWidth: 960,
         padding: `1.45rem 1.0875rem`,
       }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+    ></div>
   </header>
 )
 
