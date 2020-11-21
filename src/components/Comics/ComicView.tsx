07bf7b040e9b0ea86c980a601a6ac7d8c94aa7aa
Ebrima <ebrima.jobe92@gmail.com>
2020-11-21 02:59:53 -0800

    create comic view

---
+++ src/components/Comics/ComicView.tsx
@@ -0,0 +1,47 @@
+
+import React from 'react'
+import { useStaticQuery, graphql, Link } from 'gatsby'
+import { Grid, Comic} from './styles'
+import { Img } from '../../utils/styles'
+
+
+const ComicView = () => {
+    const { allContentfulComics } = useStaticQuery(
+    graphql`
+        query {
+          allContentfulComics {
+            edges {
+              node {
+                 id
+                 title
+                 comics {
+                    createdAt
+                    fluid {
+                        ...GatsbyContentfulFluid
+                    }
+                    sizes(maxWidth: 614) {
+                        ...GatsbyContentfulSizes_tracedSVG
+                      }
+                    }
+                  }
+                }
+              }
+        }
+        `
+    )
+    console.log(allContentfulComics)
+
+    return (<div><Grid>
+        {allContentfulComics.edges[0].node.comics.sort((a,b)=> new Date(b.createdAt).getTime()-new Date(a.createdAt).getTime()).map(node=>{
+            console.log(node)
+            return (
+                <Comic key={node.title}>
+                    <Img fluid={node.fluid} />
+                </Comic>
+
+            )
+        })}
+        
+        </Grid></div>)
+}
+export default ComicView
