5751e4175e10c94208a580807d779a6d515e047f
Ebrima <ebrima.jobe92@gmail.com>
2020-11-21 03:00:17 -0800

    create comics page

--- src/pages/comics.tsx
+++ src/pages/comics.tsx
@@ -3,13 +3,15 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import SEO from '../components/seo'
+import ComicView from '../components/Comics/ComicView'
 
 const ComicsPage = () => {
   return (
   <>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>About</h1>
-    <p>Welcome to your new Shop powered by Gatsby and Shopify.</p>
+    <SEO title="Comics" keywords={[`gatsby`, `application`, `react`]} />
+    {/* <h1>About</h1> */}
+    {/* <p>Welcome to your new Shop powered by Gatsby and Shopify.</p> */}
+    <ComicView/>
     <Link to="/page-2/">Go to page 2</Link>
   </>
 )
