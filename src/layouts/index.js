54e0002591f6e6c0a07daa67626cf114926aded1
ebrima <ebrima.jobe92@gmail.com>
2020-11-22 05:06:06 -0800

    logo gif + hamburger menu

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,20 +1,19 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { StaticQuery, graphql } from 'gatsby'
-import styled from '@emotion/styled'
-import ContextProvider from '~/provider/ContextProvider'
-
-import { GlobalStyle } from '~/utils/styles'
-import Navigation from '~/components/Navigation'
+import React from "react"
+import PropTypes from "prop-types"
+import { StaticQuery, graphql } from "gatsby"
+import styled from "@emotion/styled"
+import ContextProvider from "~/provider/ContextProvider"
+import logoGIF from "../images/logo.gif"
+import { GlobalStyle } from "~/utils/styles"
+import Navigation from "~/components/Navigation"
 
 const Wrapper = styled.div`
-  margin: 0 auto;
+  margin: 0 auto x
   max-width: 960px;
   padding: 0px 1.0875rem 1.45rem;
 `
 
-const Page = styled.div`
-`
+const Page = styled.div``
 const Layout = ({ children }) => {
   return (
     <ContextProvider>
@@ -27,23 +26,17 @@ const Layout = ({ children }) => {
                 title
               }
             }
-           file(relativePath: {in: "logo.jpg"}) {
-            childImageSharp {
-              fluid(maxWidth:500){
-                ...GatsbyImageSharpFluid
-              }
-            }
           }
-        }
         `}
         render={data => (
           <Page>
-            <Navigation logo={data.file} siteTitle={data.site.siteMetadata.title} />
+            <Navigation
+              logo={logoGIF}
+              siteTitle={data.site.siteMetadata.title}
+            />
             <Wrapper>
               {children}
-              <footer>
-              
-              </footer>
+              <footer></footer>
             </Wrapper>
           </Page>
         )}
