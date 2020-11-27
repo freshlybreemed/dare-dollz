5607267e6000a5a060ffbc67826d30f9aa5313b2
ebrima <ebrima.jobe92@gmail.com>
2020-11-27 00:41:15 -0500

    refactor

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -1,4 +1,3 @@
-import styled from '@emotion/styled'
 import React, { useContext } from 'react'
 
 
@@ -7,82 +6,8 @@ import StoreContext from '../../context/StoreContext'
 import LineItem from './LineItem'
 import { useStaticQuery, graphql } from 'gatsby'
 import { endsWith } from 'lodash'
+import { HeaderWrapper, SubHeader, Subtext, CheckoutButton, SubtotalWrapper, CloseButton, Header, Wrapper} from './styles'
 
-
-const HeaderWrapper = styled.div`
-  /* padding: 0 0.75rem 0.75rem 0.75rem; */
-  display: -webkit-flex;
-  display: -ms-flexbox;
-  display: flex;
-  -ms-flex-align: center;
-  -webkit-align-items: center;
-  -moz-align-items: center;
-  -ms-align-items: center;
-  -o-align-items: center;
-  align-items: center;
-  -webkit-justify-content: space-between;
-  -ms-justify-content: space-between;
-  justify-content: space-between;
-  border-bottom: 1px solid #d4d4d4;
-
-`
-
-const Subtext = styled.div`
-    font-size: .9rem;
-    text-align: center;
-    font-weight: 300;
-`
-
-const CheckoutButton = styled.button`
-  padding: 1.5rem;
-  background-color: black;
-  color: white;
-  text-transform: uppercase;
-  width:100%;
-  margin-top: 2rem;
-  border: 0.0625rem solid #000;
-  `
-
-const SubtotalWrapper = styled.div`
-  /* padding: 0 0.75rem 0.75rem 0.75rem; */
-  display: -webkit-flex;
-  display: -ms-flexbox;
-  display: flex;
-  -ms-flex-align: center;
-  -webkit-align-items: center;
-  -moz-align-items: center;
-  -ms-align-items: center;
-  -o-align-items: center;
-  align-items: center;
-  -webkit-justify-content: space-between;
-  -ms-justify-content: space-between;
-  justify-content: space-between;
-  border-top: 1px solid #d4d4d4;
-
-`
-
-const CloseButton = styled.div`
-  width:20px;
-`
-const Paragraph = styled.p`
-  font-size: 1.1rem;
-  line-height: 1.5;
-  padding-top:1.5rem;
-  padding-bottom:1.5rem;
-`
-const Wrapper = styled.p`
-  padding: 0 1rem;
-`
-const Header = styled.div`
-  font-size: 1.25rem;
-  font-weight: 700;
-  text-transform: uppercase;
-  line-height: 1.4;
-  /* padding-top: 1rem; */
-  padding-bottom: 1rem;
-  width:80%;
-  margin-top: 1rem;
-`
 
 const Cart = ({ setCartActive, cartActive }) => {
   const { close } = useStaticQuery(graphql`
@@ -122,7 +47,7 @@ const Cart = ({ setCartActive, cartActive }) => {
       {lineItems}
       </div>
       <SubtotalWrapper>
-        <Header>Subtotal</Header>
+        <SubHeader>Subtotal</SubHeader>
         <p>$ {checkout.subtotalPrice}</p>
       </SubtotalWrapper>
       <Subtext>
