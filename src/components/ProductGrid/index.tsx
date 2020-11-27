fa57d7042a516993fccf4e5a111d2fb9f726b6ac
ebrima <ebrima.jobe92@gmail.com>
2020-11-26 19:47:19 -0500

    useState import

--- src/components/ProductGrid/index.tsx
+++ src/components/ProductGrid/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react"
+import React, { useContext,useState } from "react"
 import { useStaticQuery, graphql, Link } from "gatsby"
 
 import StoreContext from "../../context/StoreContext"
