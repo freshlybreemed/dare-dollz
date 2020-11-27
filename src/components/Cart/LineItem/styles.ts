59cdf82d1b154599b76e079c3fd119d806ef434b
ebrima <ebrima.jobe92@gmail.com>
2020-11-27 00:03:59 -0500

    line item styles

--- src/components/Cart/LineItem/styles.ts
+++ src/components/Cart/LineItem/styles.ts
@@ -5,5 +5,15 @@ export const Wrapper = styled.div`
   justify-content: space-between;
   align-items: center;
   flex-wrap: wrap;
-  padding: 2rem 0 2rem 0;
+  padding: 1rem 0 1rem 0;
+`
+export const CloseButton = styled.div`
+width:20px;
+`
+export const Paragraph = styled.span`
+  font-size: 1.1rem;
+  line-height: 1.5;
+  padding-top:1.5rem;
+  padding-bottom:1.5rem;
+
 `
